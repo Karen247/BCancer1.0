690bc19d8b503555d613c4ee4a40c1c95c68faf0
x506247 <x506247@fi.muni.cz>
2022-04-07 08:23:01 +0200

    changes

---
+++ plot_cancer_clusters.py
@@ -0,0 +1,27 @@
+
+def plot_clusters_cancer(data, clusters):
+    """
+    Plots clusters using scatter plot and color them acordingly.
+
+    :param pd.DataFrame data: dataframe with datapoints havig columns "x" and "y"
+    :param list of int clusters: cluster label for each of the datapoint
+    """
+    import seaborn as sns
+    # find outliers not belonging to any cluster (only relevant for DBSCAN)
+    outlier_indices = clusters == -1
+
+    # plot points and color them by cluster/label
+    ax = sns.scatterplot(
+        data=data[~outlier_indices],
+        x="x",
+        y="y",
+        hue=data['diagnosis'],
+        palette="muted",
+        legend=True,
+    )
+
+    if outlier_indices.any():
+        ax = sns.scatterplot(
+            data=data[outlier_indices], x="x", y="y", color="black", ax=ax, hue = 'diagnosis'
+        )
+    return ax
